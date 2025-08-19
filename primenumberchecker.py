b5534a9b49225728a6cc8f09228fd74f3b9e280a
Eduardo-mEsteves <eduardofrancamesteves@gmail.com>
2025-08-18 22:49:51 -0300

    Update primenumberchecker.py

--- primenumberchecker.py
+++ primenumberchecker.py
@@ -1,24 +1,24 @@
-n = int(input("Number: ")) #Choose a number
-x = 1 #it will run the while loop
-dividers  = [] #it will show the dividers of that number
+number = int(input("Number: ")) #Choose a number
+counter = 1 #it will run the while loop
+divisors  = [] #it will show the divisors of that number
 
 #number of dividers
-d = 0
+divisorCount = 0
 
 #division
-r = 0 #will show the rest of the division
+remainder = 0 #will show the remainder of the division
 
-while x <= n:
-  r = 0
-  r = n % x
-  if r == 0:
-    d = d + 1
-    dividers.append(x)
+while counter <= number:
+  remainder = 0
+  remainder = number % counter
+  if remainder == 0:
+    divisorCount += 1
+    divisors.append(counter)
 
-  x += 1
+  counter += 1
 
-print(f"Dividers of {n}: {dividers}")
-if d == 2:
-  print(f"{n} is a prime number")
-elif d > 2:
-  print(f"{n} isn't a prime number")
+print(f"Dividers of {number}: {divisors}")
+if divisorCount == 2:
+  print(f"{number} is a prime number")
+elif divisorCount > 2:
+  print(f"{number} isn't a prime number")
